8fa82d2aab7e8fa7fdf4739dead06d5740acbeeb
Jessica Gleason <jessicaruthgleason@gmail.com>
2022-02-01 13:48:08 -0600

    Initial rough setup of state for current filters and current location list

--- insp-challenge/src/components/Layout/index.js
+++ insp-challenge/src/components/Layout/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from 'src/components/Grid';
 import Filter from 'src/components/Filter';
 
 export default function Layout() {
     const { tripSet, styles } = require('src/trips.json');
+    const [selectedLocations, setSelectedLocations] = useState(tripSet);
+    const [activeFilters, setActiveFilters] = useState(["Mountain"]);
+
+    const filteredTrips = tripSet.filter(item => activeFilters.includes(item.unitStyleName));
+
+    useEffect(() => {
+        setSelectedLocations(filteredTrips);
+    }, []);
 
     return (
         <main>
             <h1>Locations</h1>
             <Filter styles={styles} />
-            <Grid locations={tripSet} />
+            <Grid locations={selectedLocations} />
         </main>
     )
 }
